676c543696b9af42aefbf0c35c3df5e2028e31da
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-08-02 22:52:50 -0500

    Agrego xyz.java modificados del ejercicio 2: Estudiante y TestEstudiante

--- src/com/cmc/repaso/test/TestEstudiante.java
+++ src/com/cmc/repaso/test/TestEstudiante.java
@@ -1,9 +1,32 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Estudiante;
+
 public class TestEstudiante {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Estudiante e1 = new Estudiante("María López");
+		e1.calificar(9.5);
+		System.out.println("Estudiante: " + e1.getNombre());
+		System.out.println("Nota: " + e1.getNota());
+		System.out.println("Resultado: " + e1.getResultado());
+
+		System.out.println();
+
+		Estudiante e2 = new Estudiante("Juan Pérez");
+		e2.calificar(6.0);
+		System.out.println("Estudiante: " + e2.getNombre());
+		System.out.println("Nota: " + e2.getNota());
+		System.out.println("Resultado: " + e2.getResultado());
+
+		System.out.println();
+
+		Estudiante e3 = new Estudiante("Ana Torres");
+		e3.calificar(8.0);
+		System.out.println("Estudiante: " + e3.getNombre());
+		System.out.println("Nota: " + e3.getNota());
+		System.out.println("Resultado: " + e3.getResultado());
 
 	}
 
