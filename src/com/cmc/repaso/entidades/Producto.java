df0afbc58b5a0c066973efbffa6c0fb436a15433
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-08-02 23:13:34 -0500

    Agrego xyz.java modificados del ejercicio 3: Producto y TestProducto

--- src/com/cmc/repaso/entidades/Producto.java
+++ src/com/cmc/repaso/entidades/Producto.java
@@ -2,4 +2,43 @@ package com.cmc.repaso.entidades;
 
 public class Producto {
 
+	private String nombre;
+	private double precio;
+
+	public Producto(String nombre, double precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public void setPrecio(double precio) {
+
+		if (precio < 0) {
+			this.precio = precio * -1;
+		} else {
+			this.precio = precio;
+		}
+
+	}
+
+	public double calcularPrecioPromo(double porcentajeDescuento) {
+
+		double descuento = precio * (porcentajeDescuento / 100);
+		double precioConDescuento = precio - descuento;
+
+		return precioConDescuento;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
 }
--- src/com/cmc/repaso/test/TestProducto.java
+++ src/com/cmc/repaso/test/TestProducto.java
@@ -1,9 +1,23 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Producto;
+
 public class TestProducto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Producto p1 = new Producto("Mouse Inalámbrico", 25.0);
+		System.out.println("Precio inicial: " + p1.getPrecio());
+
+		p1.setPrecio(-30);
+		System.out.println("Precio después de setPrecio(-30): " + p1.getPrecio());
+
+		p1.setPrecio(40);
+		System.out.println("Precio después de setPrecio(40): " + p1.getPrecio());
+
+		double precioPromo = p1.calcularPrecioPromo(20);
+		System.out.println("Precio con 20% de descuento: " + precioPromo);
+		System.out.println("Precio original (sin cambios): " + p1.getPrecio());
 
 	}
 
