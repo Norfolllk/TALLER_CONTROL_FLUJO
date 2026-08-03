b27f43d2f7453cd3c097d15c901408123fa03325
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-08-02 22:43:33 -0500

    Agrego xyz.java modificados del ejercicio 1: crea Validacion y TestValidacion

--- src/com/cmc/repaso/test/TestValidacion.java
+++ src/com/cmc/repaso/test/TestValidacion.java
@@ -1,9 +1,21 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Validacion;
+
 public class TestValidacion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Validacion validacion = new Validacion();
+
+		boolean resultado1 = validacion.validarMonto(150.50);
+		System.out.println("Monto 150.50 -> ¿Es válido?: " + resultado1);
+
+		boolean resultado2 = validacion.validarMonto(0);
+		System.out.println("Monto 0 -> ¿Es válido?: " + resultado2);
+
+		boolean resultado3 = validacion.validarMonto(-50);
+		System.out.println("Monto -50 -> ¿Es válido?: " + resultado3);
 
 	}
 
